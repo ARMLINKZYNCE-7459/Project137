4d2bf52f736a245e108664f8c3656df44e46f691
ARMLINKZYNCE-7459 <96234947+ARMLINKZYNCE-7459@users.noreply.github.com>
2022-01-04 14:20:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+status = "";
+
+function setup() {
+    canvas = createCanvas(480, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    video.size(480, 380);
+}
+
+function start() {
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    }
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    }
+
+function draw() {
+    image(video, 0, 0, 480, 380);
+    if(status =! "") {
+    objectDetector.detect(video, gotResults);
+    for(i = 0; 1 < objects.length; i++) {
+        document.getElementById("status").innerHTML = "Status : Objects Detected";
+        document.getElementById("number_of_objects").innerHTML = "NUmber of Objects detected are : " + objects.length;
+        fill("#FF0000");
+        percent = floor(objects[i].confidence * 100);
+        text(objects[i].label + "" + percent + "%", objects[i].x + 15, objects[i].y + 15);
+        noFill();
+        stroke("#FF0000");
+        rect(objects[i].x, objects[i].y, objects[i].width, object[i].height);
+    }
+    }
+}
+
+function gotResults(error, results) {
+    if(error){
+        console.log(error);
+    }
+     console.log(results);
+     objects = results;
+}
